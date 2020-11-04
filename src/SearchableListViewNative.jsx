3dab6c20e33eb6a69bb430a391f2a57f3060903b
Conner Charlebois <cdcharlebois@gmail.com>
2020-11-04 15:28:38 -0500

    v1

--- src/SearchableListViewNative.jsx
+++ src/SearchableListViewNative.jsx
@@ -1,9 +1,41 @@
-import { Component, createElement } from "react";
+import { Component, createElement, useState } from "react";
+import { TextInput, View } from "react-native";
+import { SearchableFlatList } from "react-native-searchable-list";
+import { flattenStyles } from "./utils/common";
+const defaultStyle = {
+    container: {},
+    // label: {},
+    input: {}
+};
 
-import { HelloWorldSample } from "./components/HelloWorldSample";
-
-export class SearchableListViewNative extends Component {
-    render() {
-        return <HelloWorldSample sampleText={this.props.sampleText} style={this.props.style} />;
-    }
-}
+export const SearchableListViewNative = ({ datasource, content, attr, style, placeholder }) => {
+    const [searchTerm, setSearchTerm] = useState("");
+    const styles = flattenStyles(defaultStyle, style);
+    const data =
+        datasource.status !== "available"
+            ? null
+            : datasource.items.map(item => ({
+                  id: item.id,
+                  attrVal: attr(item).value,
+                  objItem: item
+              }));
+    return datasource.status !== "available" ? null : (
+        <View style={styles.container}>
+            <TextInput
+                style={styles.input}
+                placeholder={(placeholder.status = "available" ? placeholder.value : "...")}
+                value={searchTerm}
+                onChangeText={searchTerm => setSearchTerm(searchTerm)}
+            />
+            <SearchableFlatList
+                data={data}
+                searchTerm={searchTerm}
+                searchAttribute={"attrVal"}
+                ignoreCase={true}
+                renderItem={({ item }) => content(item.objItem)}
+                keyExtractor={item => item.id}
+            />
+        </View>
+    );
+};
+SearchableListViewNative.displayName = "SearchableListViewNative";
